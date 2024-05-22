56b955c880bc1244f25097d37265da70dbdd2ad9
tahani-cherif <cheriftahani92@gmail.com>
2024-05-22 21:15:28 +0100

    add crud user

---
+++ utils/apiError.js
@@ -0,0 +1,12 @@
+// @desc this class is responsible about operaion errers
+
+class ApiError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith(4) ? "fail" : "error";
+    this.isOperational = true;
+  }
+}
+
+export default ApiError;
